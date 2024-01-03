3f91fb81dc7504451849da26d97525ebb1feedb9
dydgjs200 <dydgjs2016@naver.com>
2024-01-03 12:38:51 +0900

    라우터 불필요한 주석 삭제

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,6 @@ const getUrlMiddleware = require('../middlewares/getUrl');
 const checkAccessTokenMiddleware = require('../middlewares/checkAccessToken');
 const controller = require("../controllers/community.controller");
 
-/**
- * @swagger
- * /api/test:
- *   get:
- *     summary: Get test data
- *     tags:
- *       - Test
- *     responses:
- *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             example:
- *               message: "Hello, Swagger!"
- */
-
-// 게시물 제출 페이지
-//router.get("/board", controller.board);
-//router.post("/board", controller.createCommunity);
-
-// 게시물 확인 페이지
-//router.get("/boardList", controller.readCommunity);
-//router.post("/writeComment", controller.writeComment);
-//router.post("/readComment", controller.readComment);
-
-// 임시
-//router.get("/allComment", controller.readAllComment);
-
 router.get("/community", getUrlMiddleware, controller.community);
 
 // searchCommunity 부분
